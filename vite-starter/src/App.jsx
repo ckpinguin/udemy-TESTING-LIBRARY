8e2481a6eca4d1fdb930de7a59750a4cf163fe85
Christof Kälin <christof.kaelin@protonmail.com>
2024-07-23 21:34:25 +0200

    first tries

--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -3,7 +3,7 @@ function App() {
     <div>
       <h1>I'm gonna learn React Testing Library</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,8 +1,10 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from "@testing-library/react"
+import App from "./App"
 
 test("App contains correct heading", () => {
-  render(<App />);
-  const headingElement = screen.getByText(/learn react/i);
-  expect(headingElement).toBeInTheDocument();
-});
+  render(<App />)
+  const headingElement = screen.getByRole("heading", {
+    name: /learn React Testing/i,
+  })
+  expect(headingElement).toBeInTheDocument()
+})
