2c186b651605b3ed1f66bab07a1eeb8c37dc73ec
Christof Kälin <christof.kaelin@protonmail.com>
2024-07-30 15:19:51 +0200

    more tdd practice

--- vite-starter/src/App.jsx
+++ vite-starter/src/App.jsx
@@ -3,15 +3,26 @@ import { useState } from "react"
 
 function App() {
   const [buttonColor, setButtonColor] = useState("red")
+  const [disabled, setDisabled] = useState(false)
   const newButtonColor = buttonColor === "red" ? "blue" : "red"
 
   return (
     <div>
       <button
-        className={buttonColor}
+        className={disabled ? "gray" : buttonColor}
+        disabled={disabled}
         onClick={() => setButtonColor(newButtonColor)}>
         Change to {newButtonColor}
       </button>
+      <input
+        class=""
+        type="checkbox"
+        value="Disable Button"
+        id="disable-button-checkbox"
+        defaultChecked={false}
+        onChange={() => setDisabled(!disabled)}
+      />
+      <label htmlFor="disable-button-checkbox">Disable Button</label>
     </div>
   )
 }
--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -1,22 +1,25 @@
-import { render, screen, fireEvent, logRoles } from "@testing-library/react"
+import { render, screen, fireEvent } from "@testing-library/react"
 import App from "./App"
-import { it, test, expect } from "vitest"
+import { test, expect, beforeEach } from "vitest"
 
-it("starts with correct button color", () => {
-  const { container } = render(<App />)
-  logRoles(container)
-  const buttonEl = screen.getByRole("button", { name: /blue/i }) // implicit assertion
-  expect(buttonEl).toHaveClass("red")
-})
-
-it("has the correct button text", () => {})
-
-it("has the correct button color after click", () => {})
-
-it("has the correct button text after click", () => {})
+// Prefer function setup() over beforeEach() for better maintainability and scoping
+// cf. https://kentcdodds.com/blog/avoid-nesting-when-youre-testing
+function setup() {
+  render(<App />)
+}
 
 test("button click flow", () => {
-  render(<App />)
+  setup()
+  const buttonEl = screen.getByRole("button", { name: /blue/i }) // implicit assertion
+  expect(buttonEl).toHaveClass("red")
+  //expect(buttonEl).toHaveTextContent(/blue/i) // already implicitly tested by the getByRole()
+  fireEvent.click(buttonEl)
+  expect(buttonEl).toHaveClass("blue")
+  expect(buttonEl).toHaveTextContent(/red/i)
+})
+
+test("button click flow", () => {
+  setup()
   // find an element with a role of button and text matching /blue/i
   const buttonElement = screen.getByRole("button", {
     name: /blue/i,
@@ -33,4 +36,47 @@ test("button click flow", () => {
 
   // expect the button text to match /red/i
   expect(buttonElement).toHaveTextContent(/red/i)
+})
+
+test("checkbox flow", () => {
+  setup()
+  // find elements
+  const buttonEl = screen.getByRole("button", { name: /blue/i })
+  const checkboxEl = screen.getByRole("checkbox", { name: /disable button/i })
+
+  // check initial conditions
+  expect(buttonEl).toBeEnabled()
+  expect(checkboxEl).not.toBeChecked()
+
+  // Interact
+  fireEvent.click(checkboxEl)
+
+  // check conditions after first interaction
+  expect(checkboxEl).toBeChecked()
+  expect(buttonEl).toBeDisabled()
+  expect(buttonEl).toHaveClass("gray")
+
+  // Interact
+  fireEvent.click(checkboxEl)
+
+  // check conditions after second interaction
+  expect(checkboxEl).not.toBeChecked()
+  expect(buttonEl).toBeEnabled()
+  expect(buttonEl).toHaveClass("red")
+
+  // click button
+  fireEvent.click(buttonEl)
+
+  // expect the class to be blue
+  expect(buttonEl).toHaveClass("blue")
+
+  // expect the button text to match /red/i
+  expect(buttonEl).toHaveTextContent(/red/i)
+
+  // Interact
+  fireEvent.click(checkboxEl)
+
+  expect(checkboxEl).toBeChecked()
+  expect(buttonEl).toBeDisabled()
+  expect(buttonEl).toHaveClass("gray")
 })
