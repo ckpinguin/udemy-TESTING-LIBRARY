0913010b30d68e85a863566ff91aa5ad90870815
Christof Kälin <christof.kaelin@protonmail.com>
2024-07-31 14:21:14 +0200

    code quiz

--- sundae-starter/src/App.jsx
+++ sundae-starter/src/App.jsx
@@ -1,9 +1,11 @@
 import SummaryForm from "./pages/summary/SummaryForm"
+import Options from "./pages/entry/Options"
 
 function App() {
   return (
     <div>
       <h1>Sundaes on Demand</h1>
+      <Options />
       <SummaryForm />
     </div>
   )
--- sundae-starter/src/pages/entry/Options.jsx
+++ sundae-starter/src/pages/entry/Options.jsx
@@ -1,3 +1,30 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import Row from "react-bootstrap/Row"
+import ScoopOption from "./ScoopOption"
+import ToppingOption from "./ToppingOption"
+
 export default function Options({ optionType }) {
-  return <div></div>
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((res) => setItems(res.data))
+      .catch((err) => {
+        console.error(err)
+      })
+  }, [optionType])
+
+  const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption
+
+  const optionItems = items.map((item) => (
+    <ItemComponent
+      key={item.name}
+      name={item.name}
+      imagePath={item.imagePath}
+    />
+  ))
+
+  return <Row>{optionItems}</Row>
 }
