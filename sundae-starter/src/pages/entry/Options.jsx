f1a0f9eedf7ee627e7f9246d7bf428387ba37f10
Christof Kälin <christof.kaelin@protonmail.com>
2024-07-31 15:36:35 +0200

    msw tests with server errors

--- sundae-starter/src/pages/entry/Options.jsx
+++ sundae-starter/src/pages/entry/Options.jsx
@@ -3,18 +3,24 @@ import { useEffect, useState } from "react"
 import Row from "react-bootstrap/Row"
 import ScoopOption from "./ScoopOption"
 import ToppingOption from "./ToppingOption"
+import AlertBanner from "../common/AlertBanner"
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([])
+  const [error, setError] = useState(false)
 
   useEffect(() => {
     axios
       .get(`http://localhost:3030/${optionType}`)
       .then((res) => setItems(res.data))
       .catch((err) => {
-        console.error(err)
+        setError(true)
       })
   }, [optionType])
+
+  if (error) {
+    return <AlertBanner />
+  }
 
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption
 
---
+++ sundae-starter/src/pages/entry/tests/OrderEntry.test.jsx
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react"
+import { server } from "../../../mocks/server"
+import { http, HttpResponse } from "msw"
+
+import OrderEntry from "../OrderEntry"
+import { it, expect } from "vitest"
+
+it("handles error for scoops and toppings routes", async () => {
+  server.resetHandlers(
+    http.get("http://localhost:3030/scoops", () => {
+      return new HttpResponse(null, { status: 500 })
+    }),
+    http.get("http://localhost:3030/toppings", () => {
+      return new HttpResponse(null, { status: 500 })
+    })
+  )
+
+  const { container } = render(<OrderEntry />)
+
+  const alerts = await screen.findAllByRole("alert")
+  //  name: "An unexpected error occurred. Please try again later.",
+  //})
+  /*   const alerts = await screen.findAllByText(
+    "An unexpected error occurred. Please try again later."
+  ) */
+  // logRoles(container)
+  expect(alerts).toHaveLength(2)
+})
