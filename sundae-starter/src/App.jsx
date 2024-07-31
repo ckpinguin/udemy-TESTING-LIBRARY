fb81fefa935020cb94904fb77dd51bacbdcf61e8
Christof Kälin <christof.kaelin@protonmail.com>
2024-07-31 18:04:13 +0200

    order options, summary using the orderdetails hook

--- sundae-starter/src/App.jsx
+++ sundae-starter/src/App.jsx
@@ -1,13 +1,16 @@
 import SummaryForm from "./pages/summary/SummaryForm"
 import Options from "./pages/entry/Options"
+import Container from "react-bootstrap/Container"
+import OrderEntry from "./pages/entry/OrderEntry"
+import { OrderDetailsProvider } from "./contexts/OrderDetails"
 
 function App() {
   return (
-    <div>
-      <h1>Sundaes on Demand</h1>
-      <Options />
-      <SummaryForm />
-    </div>
+    <Container>
+      <OrderDetailsProvider>
+        <OrderEntry />
+      </OrderDetailsProvider>
+    </Container>
   )
 }
 
--- sundae-starter/src/pages/summary/OrderSummary.jsx
+++ sundae-starter/src/pages/summary/OrderSummary.jsx
@@ -1,3 +1,29 @@
+import { useOrderDetails } from "../../contexts/OrderDetails"
+import SummaryForm from "./SummaryForm"
+import { formatCurrency } from "../../utilities"
+
 export default function OrderSummary() {
-  return <div></div>
+  const { totals, optionCounts } = useOrderDetails()
+
+  const scoopArr = Object.entries(optionCounts.scoops)
+  const scoopList = scoopArr.map(([key, value]) => (
+    <li key={key}>
+      {value} {key}
+    </li>
+  ))
+  const toppingArr = Object.entries(optionCounts.toppings)
+  const toppingList = toppingArr.map((topping) => (
+    <li key={topping}>{topping}</li>
+  ))
+
+  return (
+    <div>
+      <h1>Order Summary</h1>
+      <h2>Scoops: {formatCurrency(totals.scoops)}</h2>
+      <ul>{scoopList}</ul>
+      <h2>Toppings: {formatCurrency(totals.toppings)}</h2>
+      <ul>{toppingList}</ul>
+      <SummaryForm />
+    </div>
+  )
 }
